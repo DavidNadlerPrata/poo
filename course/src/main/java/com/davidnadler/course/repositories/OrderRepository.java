d64956fcc32fe5dfa5dc1b0b40fb2b03a272d571
DavidNadlerPrata <ddnprata@gmail.com>
2025-06-16 19:27:13 -0300

    Order, Instant, ISO 8601, Parte 2

---
+++ course/src/main/java/com/davidnadler/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.davidnadler.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.davidnadler.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
